c2a4d316d9eeac6ea3d90b427a7160f41cf02437
aahanaaaaaa26 <aahanaa2601@gmail.com>
2026-02-04 14:19:28 +0530

    select

--- app2.py
+++ app2.py
@@ -1,7 +1,8 @@
 import streamlit as st
 st.title("my age and city")
 age = st.number_input("Enter your age:",1,100)
-city = st.selectionbox("Select your city:",["New York","Los Angeles","Chicago","Houston","Phoenix"])
+city = st.selectbox("Select your city:",["New York","Los Angeles","Chicago","Houston","Phoenix"])
 
 if st.button("Show the details"):
+
     st.write("Your age is", age, "and you live in", city)
